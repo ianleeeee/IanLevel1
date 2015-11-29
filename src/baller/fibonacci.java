9663d4fbe67ff26c53fd8bb23353fbbc80034624
Dave <dave.dunn457@gmail.com>
2015-11-29 15:58:26 -0800

    fibonacci thing

---
+++ src/baller/fibonacci.java
@@ -0,0 +1,137 @@
+package baller;
+
+public class fibonacci {
+	public static void main(String[] args) {
+		takeIn(0);
+	}
+
+	static void takeIn(int x) {
+
+		if (x < 1) {
+
+		} else {
+			System.out.println("1");
+		}
+		if (x < 1) {
+
+		} else {
+			System.out.println("1");
+		}
+		if (x < 2) {
+
+		} else {
+			System.out.println("2");
+		}
+		if (x < 3) {
+
+		} else {
+			System.out.println("3");
+		}
+		if (x < 5) {
+
+		} else {
+			System.out.println("5");
+		}
+		if (x < 8) {
+
+		} else {
+			System.out.println("8");
+		}
+		if (x < 13) {
+
+		} else {
+			System.out.println("13");
+		}
+		if (x < 21) {
+
+		} else {
+			System.out.println("21");
+		}
+		if (x < 34) {
+
+		} else {
+			System.out.println("34");
+		}
+		if (x < 55) {
+
+		} else {
+			System.out.println("55");
+		}
+		if (x < 89) {
+
+		} else {
+			System.out.println("89");
+		}
+		if (x < 144) {
+
+		} else {
+			System.out.println("144");
+		}
+		if (x < 233) {
+
+		} else {
+			System.out.println("233");
+		}
+		if (x < 337) {
+
+		} else {
+			System.out.println("337");
+		}
+		if (x < 610) {
+
+		} else {
+			System.out.println("610");
+		}
+		if (x < 987) {
+
+		} else {
+			System.out.println("987");
+		}
+		if (x < 1597) {
+
+		} else {
+			System.out.println("1597");
+		}
+		if (x < 2584) {
+
+		} else {
+			System.out.println("2584");
+		}
+		if (x < 4181) {
+
+		} else {
+			System.out.println("4181");
+		}
+		if (x < 6765) {
+
+		} else {
+			System.out.println("6765");
+		}
+		if (x < 10946) {
+
+		} else {
+			System.out.println("10946");
+		}
+		if (x < 17711) {
+
+		} else {
+			System.out.println("17711");
+		}
+		if (x < 28657) {
+
+		} else {
+			System.out.println("28657");
+		}
+		if (x < 46368) {
+
+		} else {
+			System.out.println("46368");
+		}
+		if (x < 75025) {
+
+		} else {
+			System.out.println("75025");
+		}
+
+	}
+}
